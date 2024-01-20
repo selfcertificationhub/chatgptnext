64a61ba9becefadf66f729d2c2e286667be8a6ad
jason <selfcertificationhub@proton.me>
2024-01-20 22:37:25 +0900

    Updates

--- serviceWorkerRegister.js
+++ serviceWorkerRegister.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   console.log('location: ', window.location)
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register("/chatgptnext/serviceWorker.js").then(function (registration) {
+    navigator.serviceWorker.register(window.location.pathname + "serviceWorker.js").then(function (registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function (err) {
       console.error('ServiceWorker registration failed: ', err);
